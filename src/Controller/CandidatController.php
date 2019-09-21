6ee9c6fe230a9258757aef750fea3323883c37b1
Franck garcon <franck.garcon@gmail.com>
2019-09-21 19:20:12 +0200

    [WIP] begin->deserialisation

--- src/Controller/CandidatController.php
+++ src/Controller/CandidatController.php
@@ -7,13 +7,14 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Validation\Article;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class CandidatController extends AbstractController
 {
 	/**
 	 * @Rest\Get(
 	 *     path = "/candidats/{id}",
-	 *     name = "app_article_show",
+	 *     name = "app_candidat_show",
 	 *     requirements = {"id"="\d+"}
 	 * )
 	 * @Rest\View(
@@ -32,11 +33,24 @@ class CandidatController extends AbstractController
 
 	/**
 	 * @Rest\Post("/candidats")
-	 * @Rest\View
+	 * @Rest\View(
+	 *     StatusCode=201
+	 * )
 	 * @ParamConverter("candidat", converter="fos_rest.request_body")
 	 */
 	public function createAction(Candidat $candidat)
 	{
-		dump($candidat); die;
+//		$em = $this->getDoctrine()->getManager();
+//
+//		$em->persist($candidat);
+//		$em->flush();
+//
+//		return $this->view(
+//			$candidat,
+//			Response::HTTP_CREATED,
+//			['Location' => $this->generateUrl('app_candidat_show',
+//				['id' => $candidat->getId(),
+//				UrlGeneratorInterface::ABSOLUTE_URL])]
+//		);
 	}
 }
