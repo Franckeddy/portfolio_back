37d4c23c8b7d26ff34990d3464b6bfae852022b2
Franck garcon <franck.garcon@gmail.com>
2019-09-22 12:17:25 +0200

    [WIP]

--- src/Controller/CandidatController.php
+++ src/Controller/CandidatController.php
@@ -6,7 +6,6 @@ use App\Entity\Candidat;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Validation\Article;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class CandidatController extends AbstractController
@@ -32,25 +31,23 @@ class CandidatController extends AbstractController
 	}
 
 	/**
-	 * @Rest\Post("/candidats")
+	 * @Rest\Post(
+	 *     "/candidats"
+	 * )
 	 * @Rest\View(
 	 *     StatusCode=201
 	 * )
-	 * @ParamConverter("candidat", converter="fos_rest.request_body")
+	 * @ParamConverter(
+	 *     "candidat",
+	 *     converter="fos_rest.request_body")
 	 */
 	public function createAction(Candidat $candidat)
 	{
-//		$em = $this->getDoctrine()->getManager();
-//
-//		$em->persist($candidat);
-//		$em->flush();
-//
-//		return $this->view(
-//			$candidat,
-//			Response::HTTP_CREATED,
-//			['Location' => $this->generateUrl('app_candidat_show',
-//				['id' => $candidat->getId(),
-//				UrlGeneratorInterface::ABSOLUTE_URL])]
-//		);
+		$em = $this->getDoctrine()->getManager();
+
+		$em->persist($candidat);
+		$em->flush();
+
+		return $candidat;
 	}
 }
