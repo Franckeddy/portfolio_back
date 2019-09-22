f7662c3240f9f8a1974493c5ccd1b7926bca16b5
Franck garcon <franck.garcon@gmail.com>
2019-09-22 12:16:36 +0200

    [WIP]  create form

---
+++ src/Form/CandidatType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Candidat;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CandidatType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('adress')
+            ->add('town')
+            ->add('zipcode')
+            ->add('email')
+            ->add('date_of_birth')
+            ->add('langues')
+            ->add('licenses')
+            ->add('schools')
+            ->add('companies')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Candidat::class,
+        ]);
+    }
+}
---
+++ src/Form/SchoolType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\School;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SchoolType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('start_date')
+            ->add('end_date')
+            ->add('candidat')
+            ->add('formations')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => School::class,
+        ]);
+    }
+}
