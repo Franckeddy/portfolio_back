979e6a6b0b20484f7ba36224ce649b39319c9412
Franck garcon <franck.garcon@gmail.com>
2019-10-09 01:05:00 +0200

    install swagger

---
+++ swagger/swagger.php
@@ -0,0 +1,11 @@
+<?php
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(title="API Portfolio", version="0.1")
+ * @OA\Server(
+ *      url="http://localhost:82/api/doc",
+ *      description="Mon Api pour portfolio"
+ * )
+ */
