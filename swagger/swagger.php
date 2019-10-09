9dd6061dfcc80b182a7e3356d88a5817649315a9
Franck garcon <franck.garcon@gmail.com>
2019-10-09 20:07:00 +0200

    [WIP] config Apidoc

--- swagger/swagger.php
+++ swagger/swagger.php
@@ -5,7 +5,7 @@ use OpenApi\Annotations as OA;
 /**
  * @OA\Info(title="API Portfolio", version="0.1")
  * @OA\Server(
- *      url="http://localhost:82/api/doc",
+ *      url="http://http://localhost:82/swagger/index.html",
  *      description="Mon Api pour portfolio"
  * )
  */
